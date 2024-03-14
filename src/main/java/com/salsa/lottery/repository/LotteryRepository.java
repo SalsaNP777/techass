c2da58dd96d944e15c80d9683593ce75b22ec375
SalsaNp <salsa.nputr777@gmail.com>
2024-03-14 19:25:22 +0700

    ...

--- src/main/java/com/salsa/lottery/repository/LotteryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.salsa.lottery.repository;
-
-import com.salsa.lottery.entity.Lottery;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LotteryRepository extends JpaRepository<Lottery, String> {
-}
--- src/main/java/com/salsa/lottery/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.salsa.lottery.repository;
-
-import com.salsa.lottery.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
