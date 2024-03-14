a703875bc60f079de998b2b64126a184f5c55544
SalsaNp <salsa.nputr777@gmail.com>
2024-03-14 19:24:55 +0700

    ...

---
+++ src/main/java/com/salsa/lottery/entity/User.java
@@ -0,0 +1,29 @@
+package com.salsa.lottery.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "user")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    @Column(name = "name")
+    private String userName;
+    @Column(name = "email")
+    private String userEmail;
+    @Column(name = "address")
+    private String userAddress;
+    @Column(name = "phone_number")
+    private String phoneNumber;
+    @ManyToOne
+    @JoinColumn(name = "user_lottery_id",
+            referencedColumnName = "id")
+    private UserLottery transaction;
+}
